7e237b5bfb2cf67f15cb13fcff7ceb5a36f65cc3
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-20 19:05:14 -0400

    test: findLastIndex

--- test/getArticles.test.js
+++ test/getArticles.test.js
@@ -12,3 +12,10 @@ test("getArticles should fail", async t => {
     const articles = await getArticles("freesgengas")
     console.log(articles)
 })
+
+test("Array.findLastIndex saturday", async t => {
+    const monthDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    const dayCount = (weekDayName) => monthDays.filter(day => day === weekDayName).findLastIndex(day => day === weekDayName) + 1
+    console.log(dayCount('saturday'))
+    assert.equal(dayCount('saturday'), 1)
+})
