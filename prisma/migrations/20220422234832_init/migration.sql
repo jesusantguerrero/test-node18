b1a33736daf0ef55f34557021ab9332a182d8cac
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-22 19:52:56 -0400

    chore: add migrations

---
+++ prisma/migrations/20220422234832_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE `Selectors` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `cssSelector` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(191) NULL,
+    `selector` VARCHAR(191) NULL,
+    `active` BOOLEAN NOT NULL DEFAULT false,
+    `siteId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Site` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(191) NULL,
+    `selector` VARCHAR(191) NULL,
+    `published` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notifications` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `entity` VARCHAR(255) NOT NULL,
+    `entityId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `handlers` JSON NOT NULL,
+    `subject` VARCHAR(191) NOT NULL,
+    `message` VARCHAR(191) NOT NULL,
+    `data` JSON NOT NULL,
+    `channel` VARCHAR(191) NOT NULL DEFAULT 'email',
+    `status` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
