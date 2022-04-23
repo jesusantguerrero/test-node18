05ffd47421b3e7d7e60773b4e7856114080c0613
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-23 12:47:24 -0400

    fix(server): add cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import cors from "cors"
+import express from "express";
 import { getArticles } from "./libs/index.mjs"
 import { SiteRouter } from "./controllers/routes/index.mjs"
 import db from "./libs/db.mjs";
-import express from "express";
+const PORT =process.env.PORT || 3000 
 
 export const prisma = db;
 
 const app = express();
 
+app.use(cors())
 app.use('/api/v1/sites', SiteRouter)
 app.get('/api/v1/articles', (_req, res) => {
   res.send(getArticles())
@@ -15,6 +18,6 @@ app.get('/api/v1', (_req, res) => {
   res.send('Checker v1!')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 })
