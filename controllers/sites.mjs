618071337cd6051bc84ee10484f1d97c394d60f2
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-23 08:26:07 -0400

    feat: add save to site controller

--- controllers/sites.mjs
+++ controllers/sites.mjs
@@ -1,11 +1,17 @@
-export const siteController = (db) => {
+export const siteController = (prisma) => {
 
   const getSites =  async (prisma) => {
       return await prisma.site.findMany()
   }
   
   const saveSite = async (site) => {
-    // 
+    return await prisma.site.create({
+      data: {
+        title: site.title,
+        url: site.url,
+        selector: site.selector
+      }
+    })
   }
 
   return {
